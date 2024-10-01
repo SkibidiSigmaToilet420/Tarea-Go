a58c8d8a46b02903e62a8c76554cc5a48c794cc7
Jesus Blanco <jesusblanco1905@gmail.com>
2024-09-30 23:04:08 -0400

    Update Tarea.go

--- Tarea.go
+++ Tarea.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-func main(){
+func main() {
 
 	var num1, num2 int64 = 0, 0
 	var resultado float64 = 0
 	var resp int64 = 1
 
 	fmt.Println("Calculadora")
-	
+
 	fmt.Println("Ingrese el primer numero:")
 	fmt.Scan(&num1)
 
@@ -21,25 +21,23 @@ func main(){
 	fmt.Println("Escoja su operacion 1. suma 2. resta 3. Multiplicación 4. División")
 	fmt.Scan(&resp)
 
-	if resp == 1{
-		resultado=float64(num1+num2)
+	if resp == 1 {
+		resultado = float64(num1 + num2)
 	} else if resp == 2 {
-		resultado=float64(num1-num2)
+		resultado = float64(num1 - num2)
 	} else if resp == 3 {
-		resultado=float64(num1*num2)
+		resultado = float64(num1 * num2)
 	} else {
-		if num2==0{
+		if num2 == 0 {
 			fmt.Println("No se puede dividir entre cero")
-			
-		}else{
-			resultado=float64(num1/num2)
+
+		} else {
+			resultado = float64(num1 / num2)
 		}
 
-		
 	}
 
-	fmt.Print("El resultado es: ", resultado)
-	
-
+	fmt.Println("El resultado es: ", resultado)
+	fmt.Println("Presiona Enter para salir...")
+	fmt.Scan(&resp)
 }
-
